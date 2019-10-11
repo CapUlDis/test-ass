733757fe21fde577d6f73f573da31fcaf50769ee
CapUlDis <54175504+CapUlDis@users.noreply.github.com>
2019-10-11 17:39:35 +0300

    main.py => +json reading by POST method, flask_json => requirements

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_json import FlaskJSON
+
 app = Flask(__name__)
+FlaskJSON(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def hello():
+    return "Hello World!"
 
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    data = request.get_json(force=True)
+
+    if data['name'] == 'denchik' and data['password'] == 'foobar':
+        return 'status = 200 OK'
+    else:
+        return 'Invalid username/password'
+    
 if __name__ == '__main__':
     app.run()
