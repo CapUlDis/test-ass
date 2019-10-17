def506505784ebb66ca277b74e71aa066429ac03
CapUlDis <54175504+CapUlDis@users.noreply.github.com>
2019-10-17 18:13:28 +0300

    adding logging, handlers, http_codes

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from flask import Flask, request
-from flask_json import FlaskJSON
+import json
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
-FlaskJSON(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class ContentError(Exception):
+    pass
+class LogError(Exception):
+    pass
 
 @app.route('/login', methods=['POST'])
 def login():
+    try:
+        if request.headers['Content-Type'] == 'application/json':
+            data = json.loads(request.get_data())
+            if 'name' and 'password' in data.keys():
+                if data['name'] == 'denchik' and data['password'] == 'foobar':
+                    message = 'OK'
+                    http_code = 200
+                else:
+                    raise LogError
+            else: 
+                raise ContentError
+        else:
+            raise ContentError
+    except ContentError:
+        message = 'BAD REQUEST'
+        http_code = 400
+    except LogError:
+        message = 'FORBIDDEN'
+        http_code = 403
+        
+    return make_response(message, http_code)
     
-    data = request.get_json(force=True)
 
-    if data['name'] == 'denchik' and data['password'] == 'foobar':
-        return 'status = 200 OK'
-    else:
-        return 'Invalid username/password'
-    
 if __name__ == '__main__':
     app.run()
