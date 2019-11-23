49d1fd913a7d022f92fa4196a385eb7b09c92a55
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2019-11-23 15:36:11 +0300

    fix test_credit

--- tests/test_credits.py
+++ tests/test_credits.py
@@ -8,14 +8,15 @@ from credit import load_credits, logger
 @pytest.mark.parametrize(
     ('path', 'expected_str'),
     (
-        (os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/credential.txt', f"FileNotFoundError: no credentials.txt in {os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}: [Errno 2] No such file or directory: '/home/capuldis/repos/to-do-app/credential.txt'"),
-        (os.path.dirname(os.path.realpath(__file__)) + '/crap.txt', f'JSONDecodeError: data in credentials.txt is not json: Expecting value: line 1 column 1 (char 0)'),
+        (os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/credential.txt', 'to-do-app/credential.txt'),
+        (os.path.dirname(os.path.realpath(__file__)) + '/crap.txt', 'data in credentials.txt is not json'),
     ),
     ids = ['No credentials.txt case', 'Data in file is not json']
 )
 def test_load_credits(path, expected_str):
     with mock.patch.object(logger, 'error') as mock_error:
         load_credits(path)
-        assert 'JSONDecodeError: data in credentials.txt is not json: Expecting value: line 1 column 1 (char 0)' in mock_error.mock_calls[0]
-        
+        assert re.search(expected_str, str(mock_error.mock_calls[0].args))
+
+
 
