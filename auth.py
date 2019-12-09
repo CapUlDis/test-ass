24ecc66655d519062fec974af1a7c5e0e88b4d33
CapUlDis <capuldis@gmail.com>
2019-12-09 16:58:20 +0300

    create auth.py, add token creation function

--- auth.py
+++ auth.py
@@ -1,9 +1,16 @@
-import datetime
+import time
+from datetime import datetime, timedelta
 from jwcrypto import jwt, jwk
 
+    
+def create_token(username, exp, path_token_key):
 
-token_key = jwk.JWK(generate='oct', size=256)
+    with open(path_token_key, 'r') as f:
+        token_key = jwk.JWK.from_json(f.read())
+    
+    token = jwt.JWT(header={"alg": "HS256"},
+                    claims={"user": username, "exp": time.mktime((datetime.utcnow() + timedelta(minutes=exp)).timetuple())})
+    token.make_signed_token(token_key)
+    return token.serialize()
 
-token = jwt.JWT(header={"alg": "HS256"},
-                claims={"user": data['name'], "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)})
-token.make_signed_token(token_key)
+print(create_token('foo', 30, 'token_key.txt'))
