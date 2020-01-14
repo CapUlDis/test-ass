0560c45001afc4de2671aa2b3c4a2e2a6a870cb0
CapUlDis <capuldis@gmail.com>
2020-01-14 14:30:15 +0300

    added all files of alembic

---
+++ alembic/versions/2ddeb092cf37_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 2ddeb092cf37
+Revises: 
+Create Date: 2020-01-14 14:23:15.613798
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2ddeb092cf37'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
