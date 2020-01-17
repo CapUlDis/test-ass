c5f20cc8d184deb87b5b256a42a6bbb1033aa30f
CapUlDis <capuldis@gmail.com>
2020-01-17 17:07:50 +0300

    move installation of varenv in conftest to work properly, fix passwordhash in dbsetup.sh

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,19 @@
 import pytest
 import os
+
+
+os.environ['TDA_DB'] = 'postgresql://todoapp_user:tdapp8@localhost/todoapp_test'
+os.environ['TDA_CREDITS'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/credentials.txt'
+os.environ['TDA_TOKEN_KEY'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/token_key.txt'
+os.environ['TDA_TOKEN_EXPIRATION_MINUTES'] = '30'
+
+
 from flask import current_app
 from main import create_app, login
 
 
 @pytest.fixture
 def app():
-    os.environ['TDA_DB'] = 'postgresql://todoapp_user:tdapp8@localhost/todoapp_test'
-    os.environ['TDA_CREDITS'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/credentials.txt'
-    os.environ['TDA_TOKEN_KEY'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/token_key.txt'
-    os.environ['TDA_TOKEN_EXPIRATION_MINUTES'] = '30'
     app = create_app()
     yield app
 
