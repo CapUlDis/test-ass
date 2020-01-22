328e5ce24701ed1f0e07bef2ad8cb16bfafd44bf
CapUlDis <capuldis@gmail.com>
2020-01-22 16:29:40 +0300

    adding functions to class User in models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -16,4 +17,13 @@ class User(Base):
     def __repr__(self):
        return "<User(name='%s', passwordhash='%s', useremail='%s')>" % (
                             self.name, self.passwordhash, self.useremail)
+
+    def create_table_in_db(db_url):
+        engine = create_engine(db_url, echo=True)
+        Base.metadata.create_all(bind=engine)
+
+    def add_new_user_in_db(self, db_url, name, passwordhash, useremail):
+        new_user = self(name=name, passwordhash=passwordhash, useremail=useremail)
+        db_session = sessionmaker(bind=create_engine(db_url, echo=True))()
+        db_session.add(new_user).commit()
 
