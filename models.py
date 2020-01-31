a34d372107821f5947bed3839ee8452ce2c88c6a
CapUlDis <capuldis@gmail.com>
2020-01-31 12:08:02 +0300

    Add to test_credits adding and deleting user in test database.

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from flask import current_app
 
 
 Base = declarative_base()
@@ -17,9 +16,4 @@ class User(Base):
     def __repr__(self):
         return f'<User(name={self.name}, passwordhash={self.passwordhash}, useremail={self.useremail})>'
 
-    def add_new_user_in_db(self, name, passwordhash, useremail):
-        new_user = self.__clasls__(name=name, passwordhash=passwordhash, useremail=useremail)
-        db_session = current_app.Session()
-        db_session.add(new_user)
-        db_session.commit()
-
+  
--- tests/test_credits.py
+++ tests/test_credits.py
@@ -1,6 +1,7 @@
-import pytest, os
-from credit import check_user_with_password_exists, logger
-from unittest import mock
+import pytest
+from credit import check_user_with_password_exists
+from flask import current_app
+from models import User
 
 
 @pytest.mark.parametrize(
@@ -15,4 +16,10 @@ from unittest import mock
 )
 def test_check_user_with_password_exists(app, name, password, result):
     with app.app_context():
+        test_session = current_app.Session()
+        test_user = User(name='denchik', passwordhash='pbkdf2:sha256:150000$wHwsgiLd$6979f267446c0e3d2797c21006f9272c3e19d5c70925d87989983ab3826350d8', useremail='foo@bar.baz')
+        test_session.add(test_user)
+        test_session.commit()
         assert check_user_with_password_exists(name, password) is result
+        test_session.delete(test_user)
+        test_session.commit()
