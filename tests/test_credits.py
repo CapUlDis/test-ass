6e68aa1452ee0cb43eb5a2c96e24f2bb48f4adfe
CapUlDis <capuldis@gmail.com>
2020-02-13 15:23:44 +0300

    Exceptions and tests for register feature. (Postgres freezes on tests).

--- tests/test_credits.py
+++ tests/test_credits.py
@@ -2,9 +2,12 @@ import pytest
 from unittest import mock
 from flask import current_app
 from sqlalchemy import create_engine
+from sqlalchemy.sql import exists
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import and_
 from alembic import command
-from credit import check_user_with_password_exists, logger
+from credit import check_user_with_password_exists, logger, check_useremail_exist, add_new_user_to_db
+from models import User
 from .test_migrations import alembic_cfg
 
 
@@ -38,7 +41,7 @@ def test_check_user_with_password_exists_missing_db(app):
     with pytest.raises(ConnectionError):
         with mock.patch.object(logger, 'error') as mock_error:
             with app.app_context():
-                current_app.engine = create_engine('postgresql://some_user@localhost/missing_db')
+                current_app.engine = create_engine('postgresql://todoapp_user@localhost/missing_db')
                 current_app.Session = sessionmaker(bind=current_app.engine)
                 check_user_with_password_exists('foo', 'bar')
                 assert 'OperationalError: database does not exist' in mock_error.call_args[0][0]
@@ -49,3 +52,36 @@ def test_check_user_with_password_exists_missing_table(app, del_table_users_and_
             with app.app_context():
                 check_user_with_password_exists('foo', 'bar')
                 assert 'ProgrammingError: table users does not exist' in mock_error.call_args[0][0]
+
+@pytest.mark.parametrize(
+    ('useremail', 'result'),
+    (
+        ('foo@bar.baz', True),
+        ('foo@bad.baz', False)
+    ),
+    ids = ['Existing email', 'Non-existing email']
+)
+def test_check_useremail_exist_true_case(app, set_db, useremail, result):
+    assert check_useremail_exist(useremail) is result
+
+def test_check_useremail_missing_db(app):
+    with pytest.raises(ConnectionError):
+        with mock.patch.object(logger, 'error') as mock_error:
+            with app.app_context():
+                current_app.engine = create_engine('postgresql://todoapp_user@localhost/missing_db')
+                current_app.Session = sessionmaker(bind=current_app.engine)
+                check_useremail_exist('foo@bar.baz')
+                assert 'OperationalError: database does not exist' in mock_error.call_args[0][0]
+
+def test_check_useremail_missing_table(app, del_table_users_and_return_after_test):
+    with pytest.raises(NameError):
+        with mock.patch.object(logger, 'error') as mock_error:
+            with app.app_context():
+                check_useremail_exist('foo@bar.baz')
+                assert 'ProgrammingError: table users does not exist' in mock_error.call_args[0][0]
+
+def test_add_new_user_to_db(app):
+    with app.app_context():
+        add_new_user_to_db('foo', 'test_pwhash', 'bar@bax.ru')
+        test_session = current_app.Session()
+        assert test_session.query(exists().where(and_(User.name == 'foo', User.useremail == 'bar@bax.ru'))).scalar()
