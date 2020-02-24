74b848044fe745ca94fb3ed0ce08813ee99c4519
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2020-02-24 19:37:41 +0300

    Change check_useremail_exist to check_name_exist funt in credit and tests according to unique name column in DB.

--- tests/test_credits.py
+++ tests/test_credits.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import exists
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import and_
 from alembic import command
-from credit import check_user_with_password_exists, logger, check_useremail_exist, add_new_user_to_db
+from credit import check_user_with_password_exists, logger, check_name_exist, add_new_user_to_db
 from models import User
 from .test_migrations import alembic_cfg
 
@@ -54,30 +54,30 @@ def test_check_user_with_password_exists_missing_table(app, del_table_users_and_
                 assert 'ProgrammingError: table users does not exist' in mock_error.call_args[0][0]
 
 @pytest.mark.parametrize(
-    ('useremail', 'result'),
+    ('name', 'result'),
     (
-        ('foo@bar.baz', True),
-        ('foo@bad.baz', False)
+        ('denchik', True),
+        ('marat', False)
     ),
-    ids = ['Existing email', 'Non-existing email']
+    ids = ['Existing name', 'Non-existing name']
 )
-def test_check_useremail_exist_true_case(app, set_db, useremail, result):
-    assert check_useremail_exist(useremail) is result
+def test_check_name_exist_true_case(app, set_db, name, result):
+    assert check_name_exist(name) is result
 
-def test_check_useremail_missing_db(app):
+def test_check_name_missing_db(app):
     with pytest.raises(ConnectionError):
         with mock.patch.object(logger, 'error') as mock_error:
             with app.app_context():
                 current_app.engine = create_engine('postgresql://todoapp_user@localhost/missing_db')
                 current_app.Session = sessionmaker(bind=current_app.engine)
-                check_useremail_exist('foo@bar.baz')
+                check_name_exist('denchik')
                 assert 'OperationalError: database does not exist' in mock_error.call_args[0][0]
 
-def test_check_useremail_missing_table(app, del_table_users_and_return_after_test):
+def test_check_name_missing_table(app, del_table_users_and_return_after_test):
     with pytest.raises(NameError):
         with mock.patch.object(logger, 'error') as mock_error:
             with app.app_context():
-                check_useremail_exist('foo@bar.baz')
+                check_name_exist('denchik')
                 assert 'ProgrammingError: table users does not exist' in mock_error.call_args[0][0]
 
 def test_add_new_user_to_db(app):
